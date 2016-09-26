292175442b2dd6620a8080fe54051f1adbb44b3e
Josh Owens <joshua.owens@gmail.com>
2016-09-26 14:42:45 -0400

    Add a real PlacesList component

--- imports/routes.js
+++ imports/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Overseer from '/imports/ui/containers/App.js';
 import NavBar from '/imports/ui/components/NavBar.js';
-const Places = () => { return <div>Test</div> }
+import PlacesList from '/imports/ui/components/PlacesList.js';
 
 const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={ Overseer }>
-      <Route component={ NavBar}>
-        <IndexRoute component={ Places }/>
+      <Route component={ NavBar }>
+        <IndexRoute component={ PlacesList }/>
       </Route>
     </Route>
   </Router>
